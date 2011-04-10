beee758ca2a4cd1db8f17955853f098106b3842e
Phillip Cohen <pc@phillipcohen.net>
2011-04-09 21:19:33 -0500

    Created a really small project to fill in BuildInfo.cs during the automated build.

---
+++ BuildPrepare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BuildPrepare
+{
+    class Program
+    {
+        const string fileName = "LANdrop/BuildInfo.cs";
+        const string tempFileName = "LANdrop/BuildInfo.old.cs";
+
+        const string versionDef = @"public const int BUILD_NUMBER = ";
+        const string channelDef = @"public const UpdateChannels BUILD_TYPE = UpdateChannels.";
+
+        static void Main( string[] args )
+        {
+            if ( args.Length < 2 )
+            {
+                Console.WriteLine( "Usage: LDprepare buildNumber channel" );
+                return;
+            }
+
+            if ( !File.Exists( fileName ) )
+            {
+                Console.WriteLine( fileName + " does not exist" );
+                return;
+            }
+
+            File.Delete( tempFileName );
+            File.Move( fileName, tempFileName );
+
+            using ( StreamReader reader = new StreamReader( tempFileName ) )
+            using ( StreamWriter writer = new StreamWriter( fileName ) )
+            {
+                while ( !reader.EndOfStream )
+                {
+                    string line = reader.ReadLine( );
+
+                    if ( line.Trim( ).StartsWith( versionDef ) )
+                        line = versionDef + args[0] + ";";
+                    if ( line.Trim( ).StartsWith( channelDef ) )
+                        line = channelDef + args[1] + ";";
+
+                    Console.WriteLine( line );
+                    writer.WriteLine( line );
+                }
+            }
+        }
+    }
+}
