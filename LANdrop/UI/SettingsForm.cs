366f0b98bb4bb7ac675bb9eab30443680f6f4120
Phillip Cohen <pc@phillipcohen.net>
2011-04-13 23:49:31 -0500

    Added a dropdown to choose the automatic update channel to SettingsForm.

--- LANdrop/UI/SettingsForm.cs
+++ LANdrop/UI/SettingsForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using LANdrop.Updates;
 
 namespace LANdrop.UI
 {
@@ -13,18 +14,33 @@ namespace LANdrop.UI
         public SettingsForm( )
         {
             InitializeComponent( );
-            LoadFromConfiguration( Configuration.Instance );
+            LoadFromConfiguration( Configuration.Instance );            
         }
 
         private void LoadFromConfiguration( Configuration config )
         {
             tbUserName.Text = config.Username;
+            cbAutomaticUpdates.Checked = ( config.UpdateChannel != Channel.None );
+            cbReleaseChannel.Text = config.UpdateChannel.ToString();
+            if ( string.IsNullOrEmpty( cbReleaseChannel.Text ) )
+                cbReleaseChannel.Text = "Release";
+            UpdateState( );
             Refresh( );
         }
 
         private void SaveToConfiguration( Configuration config )
         {
+            if ( cbAutomaticUpdates.Checked )
+                config.UpdateChannel = (Channel) Enum.Parse( typeof( Channel ), cbReleaseChannel.Text );
+            else
+                config.UpdateChannel = Channel.None;
+
             config.Username = tbUserName.Text;
+        }
+
+        private void UpdateState( )
+        {
+            cbReleaseChannel.Enabled = cbAutomaticUpdates.Checked;
         }
 
         private void btnSave_Click( object sender, EventArgs e )
@@ -37,6 +53,11 @@ namespace LANdrop.UI
         {
             if ( MessageBox.Show( "Are you sure you want to load the default settings?", "LANdrop Configuration", MessageBoxButtons.YesNo, MessageBoxIcon.Question ) == DialogResult.Yes )
                 LoadFromConfiguration( Configuration.DefaultSettings );
+        }
+
+        private void cbAutomaticUpdates_CheckedChanged( object sender, EventArgs e )
+        {
+            UpdateState( );
         }
     }
 }
