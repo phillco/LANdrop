4a36f74e596c7640df9e0691b4804679abe94065
Phillip Cohen <pc@phillipcohen.net>
2011-07-02 09:47:13 -0600

    Caught some network-related exceptions in WebSever that occur when the remote user hangs up.

--- LANdrop/Networking/WebServer.cs
+++ LANdrop/Networking/WebServer.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using HybridDSP.Net.HTTP;
 using System.IO;
 using LANdrop.Properties;
+using System.Net.Sockets;
 
 namespace LANdrop.Networking
 {
@@ -18,11 +19,11 @@ namespace LANdrop.Networking
 
         public void HandleRequest( HTTPServerRequest request, HTTPServerResponse response )
         {
-            var matcher = UrlMatcher.Match(request.URI);
-            
+            var matcher = UrlMatcher.Match( request.URI );
+
             if ( matcher.Success )
             {
-                WebServedFile file = WebServedFile.ActiveFiles.Find(find => find.UniqueId == matcher.Groups[1].Captures[0].Value);
+                WebServedFile file = WebServedFile.ActiveFiles.Find( find => find.UniqueId == matcher.Groups[1].Captures[0].Value );
 
                 if ( matcher.Groups[2].Captures.Count > 0 && matcher.Groups[2].Captures[0].Value != "" )
                     sendFile( file, response );
@@ -38,13 +39,17 @@ namespace LANdrop.Networking
         private void sendFileInfoPage( WebServedFile file, HTTPServerResponse response )
         {
             response.ContentType = "text/html";
-            using ( Stream ostr = response.Send( ) )
-            using ( TextWriter tw = new StreamWriter( ostr ) )
-                tw.WriteLine( Resources.sendFilePage.Replace( "${fileUrl}", file.GetDownloadPath( ) )
-                    .Replace( "${fileName}", file.File.Name ) // I should be shot for chaining these together...but with sendFile.html under 500 bytes, 
-                    .Replace( "${sender}", Configuration.CurrentSettings.Username ) // the performance hit is pretty negligible.
-                    .Replace( "${fileSize}", Util.FormatFileSize( file.File.Length )));
-
+            try
+            {
+                using ( Stream ostr = response.Send( ) )
+                using ( TextWriter tw = new StreamWriter( ostr ) )
+                    tw.WriteLine( Resources.sendFilePage.Replace( "${fileUrl}", file.GetDownloadPath( ) )
+                        .Replace( "${fileName}", file.File.Name ) // I should be shot for chaining these together...but with sendFile.html under 500 bytes, 
+                        .Replace( "${sender}", Configuration.CurrentSettings.Username ) // the performance hit is pretty negligible.
+                        .Replace( "${fileSize}", Util.FormatFileSize( file.File.Length ) ) );
+            }
+            catch ( IOException ) { }
+            catch ( SocketException ) { } // User hung up (closed their browser).
         }
 
         /// <summary>
@@ -55,21 +60,26 @@ namespace LANdrop.Networking
             response.ContentLength = file.File.Length;
             response.Set( "Content-Disposition", "attachment; filename=\"" + file.File.Name + "\"" );
 
-            using ( Stream outStream = response.Send( ) )
-            using ( FileStream fileInStream = new FileStream( file.File.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) )
+            try
             {
-                // Iterate through the file in chunk-sized increments.
-                for ( long i = 0; i < file.File.Length; i += Protocol.TransferChunkSize )
+                using ( Stream outStream = response.Send( ) )
+                using ( FileStream fileInStream = new FileStream( file.File.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite ) )
                 {
-                    // Calculate the number of bytes we're about to send.
-                    int numBytes = (int) Math.Min( Protocol.TransferChunkSize, file.File.Length - i );
+                    // Iterate through the file in chunk-sized increments.
+                    for ( long i = 0; i < file.File.Length; i += Protocol.TransferChunkSize )
+                    {
+                        // Calculate the number of bytes we're about to send.
+                        int numBytes = (int) Math.Min( Protocol.TransferChunkSize, file.File.Length - i );
 
-                    // Read in the chunk from a file, write it to the network.
-                    byte[] chunk = new byte[numBytes];
-                    fileInStream.Read( chunk, 0, numBytes );
-                    outStream.Write( chunk, 0, chunk.Length );
+                        // Read in the chunk from a file, write it to the network.
+                        byte[] chunk = new byte[numBytes];
+                        fileInStream.Read( chunk, 0, numBytes );
+                        outStream.Write( chunk, 0, chunk.Length );
+                    }
                 }
             }
+            catch ( IOException ) { }
+            catch ( SocketException ) { } // User hung up (closed their browser).
         }
     }
 
